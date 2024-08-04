656505c3911a25df8c4562ae8caf288c346d1786
tube0330 <zien0516@naver.com>
2024-08-04 15:36:04 +0900

    240804_QuitBtn

--- Assets/02.Scripts/Manager/MoveSceneManager.cs
+++ Assets/02.Scripts/Manager/MoveSceneManager.cs
@@ -4,9 +4,17 @@ using UnityEngine.UI;
 
 public class MoveManager : MonoBehaviour
 {
-
     public void LoadNextScene()
     {
         SceneManager.LoadScene("PlayScene"); // 로드할 씬 이름
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif              //전처리기 지시어 끝
     }
 }
