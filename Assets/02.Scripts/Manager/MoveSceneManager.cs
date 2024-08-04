56bb7d32429da035e2d0c90b73e845af4e3ef189
tube0330 <zien0516@naver.com>
2024-08-04 15:24:49 +0900

    240804_Complete StartScene

--- Assets/02.Scripts/Manager/MoveSceneManager.cs
+++ Assets/02.Scripts/Manager/MoveSceneManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MoveSceneManager : MonoBehaviour
+public class MoveManager : MonoBehaviour
 {
 
     public void LoadNextScene()
